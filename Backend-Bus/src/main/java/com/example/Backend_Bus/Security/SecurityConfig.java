c9be011a204ebba501f9e866e9d5c84dc3c7b3a1
Chupetin <wjra31302@gmail.com>
2026-08-31 23:01:17 -0500

    config: Agrega URLs para produccion

--- Backend-Bus/src/main/java/com/example/Backend_Bus/Security/SecurityConfig.java
+++ Backend-Bus/src/main/java/com/example/Backend_Bus/Security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
@@ -25,20 +24,23 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // origen de React
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); //Para implementar mas endpoints
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",   // local
+                "https://bus-system-seven.vercel.app" //vercel
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config); // aplica a todos los endpoints
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // ← agrega esto
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
--- Frontend-Bus/src/services/AuthServices.jsx
+++ Frontend-Bus/src/services/AuthServices.jsx
@@ -1,5 +1,4 @@
-const BASE_URL = "http://localhost:8080";
-
+const BASE_URL = "https://bus-system-production.up.railway.app";
 
 export const login = async (username, password) => {
     const response = await fetch(`${BASE_URL}/auth/login`, {
